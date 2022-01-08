0b10694dac3e98eb5b32399b83ffd7b1f7fcac23
emmy70 <emmanuelusman701@gmail.com>
2022-01-08 19:58:43 +0100

    Base file & folder setup

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+// Exporting all components
+export { defaul as Loader } from './Loader';
+export { defaul as Navbar } from './Navbar';
+export { defaul as Footer } from "./Footer";
+export { defaul as Welcome } from "./Welcome";
+export { defaul as Transactions } from "./Transactions";
+export { defaul as Services } from "./Services";
