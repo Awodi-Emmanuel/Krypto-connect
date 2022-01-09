cef051d47ab691de711f2ef71a88d837d003adf4
emmy70 <emmanuelusman701@gmail.com>
2022-01-09 12:47:11 +0100

    form-card  initalized

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,8 @@ import { BsInfoCircle } from 'react-icons/bs';
 
 import { Loader } from './';
 
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-white text-white";
+
 const Welcome = () => {
 
   const connectWallet = () => {
@@ -20,9 +22,25 @@ const Welcome = () => {
             Explore the crypto world, Buy and sell cryptocurrencies easily on
             krypto.
           </p>
-          <button type="button" onClick={connectWallet}></button>
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center intems-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <p className="text-white text-base font-semibold">Connect Wallet</p>
+          </button>
+
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={commonStyles}>Low fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
         </div>
       </div>
+      <div className=""></div>
     </div>
   );
 }
